7f184eda87ccb99f12167f1fda7441e8acd62b67
lucasrls98 <lucasrls98@gmail.com>
2023-02-16 01:34:56 -0300

    Add project files.

---
+++ telaDeCadastro/ListaEndereco.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace telaDeCadastro
+{
+    public class ListaEndereco : INotifyPropertyChanged
+    {
+        string endereco;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Endereco
+        {
+            get => endereco;
+            set
+            {
+                if (endereco != value)
+                {
+                    endereco= value;
+                    NotifyPropertyChanged();
+                    
+                }
+            }
+
+        }
+        public void NotifyPropertyChanged([CallerMemberName] string propName = null)
+            //pega o nome da propriedade e coloca no parâmetro propName
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ telaDeCadastro/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace telaDeCadastro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<ListaEndereco> ListaEnderecos { get; set; }
+        //gera evento quando adiciona e quando remove endereco
+        
+        public MainWindow()
+        {
+            
+
+            ListaEnderecos = new ObservableCollection<ListaEndereco>();
+            ListaEnderecos.Add(new ListaEndereco { Endereco=""});
+
+            InitializeComponent();
+        }
+    }
+}
